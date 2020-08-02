27947d2d37f15049598bbd7c24a0d23a586098c1
TimothyWang56 <timothywang56@gmail.com>
2020-08-02 17:14:30 -0400

    get scrambleType form local storage

--- scripts/popup.js
+++ scripts/popup.js
@@ -5,14 +5,17 @@ const setScrambleText = (text) => {
 }
 
 const fetchScramble = async () => {
-    fetch('https://scrambler-api.herokuapp.com/3x3x3')
-        .then(res => res.text())
-        .then(result => {
-            const scramblesArray = JSON.parse(result);
-            scramble.innerHTML = scramblesArray[0];
+    chrome.storage.sync.get('scrambleType', function(data) {
+        const scrambleType = data.scrambleType ? data.scrambleType : '3x3x3';
+        fetch('https://scrambler-api.herokuapp.com/' + scrambleType)
+            .then(res => res.text())
+            .then(result => {
+                const scramblesArray = JSON.parse(result);
+                scramble.innerHTML = scramblesArray[0];
 
-        }
-    );
+            }
+        );
+    })
 }
 
 fetchScramble();
