6625f1305112543b19cb671f1a57ee2a1d9b8402
TimothyWang56 <timothywang56@gmail.com>
2020-08-05 14:24:02 -0400

    remove setting in local storage

--- scripts/background.js
+++ scripts/background.js
@@ -1,10 +1,6 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({scrambleType: '3x3x3'}, function() {
-        console.log("scrambleType set to 3x3x3.");
-    });
-    
+chrome.runtime.onInstalled.addListener(function() {    
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
